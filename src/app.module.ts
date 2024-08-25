29f889a8aa6319f94f39cf1628579698b463bb67
corbet91 <phunghung0901xx@gmail.com>
2024-08-25 16:56:48 +0700

    init database

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,12 @@ import { User } from './users/user.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'data.db',
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      database: 'myDatabase',
+      username:'root',
+      password: 'root',
       entities: [User],
       synchronize: true,
     }),
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './jwt/jwt.strategy';
+
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey', // Change this to an environment variable
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
